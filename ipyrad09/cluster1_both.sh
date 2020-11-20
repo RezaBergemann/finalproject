a39cebd1a337bd59c72c7f2abf63dabdb15244ee
Reza Bergemann <reza.bergemann@yale.edu>
2020-11-19 19:41:09 -0500

    Clustering completed

--- ipyrad09/cluster1_both.sh
+++ ipyrad09/cluster1_both.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=cluster1_both
 #SBATCH --time=7-
 #SBATCH --ntasks=16
-#SBATCH --mem-per-cpu=12G
+#SBATCH --mem-per-cpu=20G
 #SBATCH --cpus-per-task=2
 #SBATCH --output=cluster1_both_out.txt
 #SBATCH --error=cluster1_both_err.txt
@@ -13,4 +13,4 @@ conda activate ipyrad09
 ipcluster start --n $SLURM_NTASKS --daemonize
 
 sleep 60
-ipyrad -p params-phys_both.txt -d -s 3 -f --ipcluster -c $(($SLURM_NTASKS1))
+ipyrad -p params-phys_both.txt -d -s 3 -f --ipcluster -c $(($SLURM_NTASKS-1))
