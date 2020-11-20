47fc5c881392ea317663928bca02b9db73b26075
Reza Bergemann <reza.bergemann@yale.edu>
2020-11-19 23:55:09 -0500

    starting consensus step

---
+++ ipyrad09/consensus_both.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=consensus_both
+#SBATCH --time=1-
+#SBATCH --ntasks=16
+#SBATCH --mem-per-cpu=20G
+#SBATCH --cpus-per-task=2
+#SBATCH --output=consensus_both_out.txt
+#SBATCH --error=consensus_both_err.txt
+
+module load miniconda
+conda activate ipyrad09
+
+ipcluster start --n $SLURM_NTASKS --daemonize
+
+sleep 60
+ipyrad -p params-phys_both.txt -d -s 4 -f --ipcluster -c $(($SLURM_NTASKS))
