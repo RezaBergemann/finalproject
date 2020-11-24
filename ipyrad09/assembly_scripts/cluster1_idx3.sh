62806cd93356dc34fd2440348587289cdae2d10d
Reza Bergemann <reza.bergemann@yale.edu>
2020-11-24 13:58:52 -0500

    started new assembly with fewer bermuda samples

---
+++ ipyrad09/assembly_scripts/cluster1_idx3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=cluster1_idx3
+#SBATCH --time=3-
+#SBATCH --ntasks=18
+#SBATCH --mem-per-cpu=12G
+#SBATCH --cpus-per-task=2
+#SBATCH --output=outfiles/cluster1_idx3_out.txt
+#SBATCH --error=outfiles/cluster1_idx3_err.txt
+
+module load miniconda
+conda activate ipyrad09
+
+ipcluster start --n $SLURM_NTASKS --daemonize
+
+sleep 60
+ipyrad -p params-phys_idx3.txt -d -s 3 -f --ipcluster -c $(($SLURM_NTASKS))
