cf1be23d36321fea027b247749446960e5aa301e
Reza Bergemann <reza.bergemann@yale.edu>
2020-11-25 11:41:55 -0500

    changed script to run on dunn lab partition

--- ipyrad09/assembly_scripts/cluster1_idx3.sh
+++ ipyrad09/assembly_scripts/cluster1_idx3.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=cluster1_idx3
 #SBATCH --time=3-
+#SBATCH --partition=pi_dunn
 #SBATCH --ntasks=16
-#SBATCH --mem-per-cpu=18G
+#SBATCH --mem-per-cpu=5G
 #SBATCH --cpus-per-task=2
 #SBATCH --output=outfiles/cluster1_idx3_out.txt
 #SBATCH --error=outfiles/cluster1_idx3_err.txt
