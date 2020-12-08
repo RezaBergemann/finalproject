d6ed60fe0f464b6913c89ece9a3269216cc01090
Reza Bergemann <reza.bergemann@yale.edu>
2020-12-08 15:32:19 -0500

    finished unmasked assembly

---
+++ ipyrad09/assembly_scripts/consensus_unmasked.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=consensus_unmasked
+#SBATCH --partition=scavenge
+#SBATCH --time=4:00:00
+#SBATCH --ntasks=18
+#SBATCH --mem-per-cpu=16G
+#SBATCH --cpus-per-task=2
+#SBATCH --output=outfiles/consensus_unmasked_out.txt
+#SBATCH --error=outfiles/consensus_unmasked_err.txt
+
+module load miniconda
+conda activate ipyrad09
+
+ipcluster start --n $SLURM_NTASKS --daemonize
+
+sleep 60
+ipyrad -p params-phys_unmasked.txt -d -s 5 -f --ipcluster -c $(($SLURM_NTASKS))
